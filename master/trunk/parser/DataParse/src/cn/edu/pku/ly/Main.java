1dd5470571305fb211b657aad8de953d1d73d68b
LY <772696417@qq.com>
2015-01-06 12:25:12 +0800

    - remove some unused codes

--- master/trunk/parser/DataParse/src/cn/edu/pku/ly/Main.java
+++ master/trunk/parser/DataParse/src/cn/edu/pku/ly/Main.java
@@ -1,21 +1,8 @@
 package cn.edu.pku.ly;
 
-import java.io.File;
-import java.io.IOException;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-
 public class Main {
 
-	public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
+	public static void main(String[] args) {
 		
 		DataParse parse = new DataParse();
 		parse.startParse();
